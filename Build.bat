f8e7cb30fc8e41042a0b0d7483d328ad4d2697b9
Luke <39099814+CVFireDragon@users.noreply.github.com>
2018-12-26 15:24:18 -0500

    update build: the sequel

--- Build.bat
+++ Build.bat
@@ -1,10 +1,12 @@
 @echo off
 echo IMPORTANT! Place keys.dat at "BuildTools/keys.dat"!
+echo.
 pause
 del ExeFs\main >nul 2>nul
 mkdir binaries
 make
 copy binaries\exefsman.nso ExeFs\main
+echo.
 BuildTools\hbp\hacbrewpack.exe -k BuildTools\keys.dat --exefsdir=ExeFs --noromfs --nologo --controldir=Control --nspdir=binaries
 ren binaries\0104020706000112.nsp exefsman.nsp
 del binaries\exefsman.elf >nul 2>nul
@@ -12,4 +14,5 @@ del binaries\exefsman.pfs0 >nul 2>nul
 del binaries\exefsman.nso >nul 2>nul
 del binaries\exefsman.nacp >nul 2>nul
 echo Done Building! You can find your compiled Binaries in the 'binaries' folder.
+echo.
 pause
